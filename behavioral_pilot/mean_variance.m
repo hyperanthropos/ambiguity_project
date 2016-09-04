7729e2842e59118e7769f4952a33df6c95918c51
hyperanthropos <sebastian.weissengruber@gmail.com>
2016-09-04 20:24:25 +0200

    "added function to calculate variance"

---
+++ behavioral_pilot/mean_variance.m
@@ -0,0 +1,37 @@
+function [ mvar, ev ] = mean_variance( varargin )
+% function to calculate mean variance and expected value
+%   calculates the variance and expected value of probabilistic offers 
+%   for multiple probablities and values
+%   input = p1, v1, p2, v2, ... pn, vn (probabilities first)
+
+% convert input arguments to vector
+varargin = cell2mat(varargin);
+
+% check if input arguments are an even number
+if mod(nargin, 2) ~= 0
+    error('not a even number of input arguments');
+end
+
+% sort probabilites and values into 2 vectors
+probabilities = varargin(1, 1:2:nargin);
+values = varargin(1, 2:2:nargin);
+
+% check if probabilites are below 1
+if max(probabilities) > 1;
+    error('some probabilities are above 1 - entered values numbers in worng order?');
+end
+      
+% calculate expected value and mean variance      
+for i = 1:nargin/2
+    ev_vector(i) = values(i)*probabilities(i);
+    mvar_vector(i) = ( values(i)-ev_vector(i) )^2 * probabilities(i);
+end
+
+mvar = sum(mvar_vector);
+ev = sum(ev_vector);
+
+% end function
+end
+
+
+
