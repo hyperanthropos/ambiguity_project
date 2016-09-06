eb1d1e49645e08116da7fd68be3611c743caba7b
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2016-09-06 15:08:03 +0200

    "created presentation script"

---
+++ behavioral_pilot/presentation.m
@@ -0,0 +1,8 @@
+%% code to present the experiment
+% dependencies: stimuli.m, mean_variance.m
+clear; close all; clc;
+
+%% CREATE STIMULI MATRIX
+
+stim_mat = stimuli(0, 12, 3, 1);    % reveal ambiguity, steps, repeats, diagnostics
+
