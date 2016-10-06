addda9b2a31c6feb652cc564441da23aee080151
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2016-10-06 18:06:31 +0200

    "updated settings variables"

--- behavioral_pilot/presentation_wrapper.m
+++ behavioral_pilot/presentation_wrapper.m
@@ -12,8 +12,9 @@ SETTINGS.TEST_FLAG = 1;
 % if activated button mappings for linux (not windows, as default) are used
 SETTINGS.LINUX_MODE = 1; 
 
-target_path = 'N:\client_write\SFW_ambiguity\results'; % copies to a windows machine
-participant_nr = sub_nr;
+%%% fixed settings
+TARGET_PATH = 'N:\client_write\SFW_ambiguity\results'; % copies to a windows machine
+PARTICIPANT_NR = sub_nr;
 
 %% PREPARE AND CONFRIM
 
@@ -21,9 +22,9 @@ participant_nr = sub_nr;
 home = pwd;
 savedir = fullfile(home, 'logfiles');
 if exist(savedir, 'dir') ~= 7; mkdir(savedir); end % create savedir if it doesn't exist
-save_file_0 = fullfile(savedir, [ 'part_' sprintf('%03d', participant_nr) '_sess_' num2str(0) '_ambiguity_' num2str(AMBIGUITY)  '.mat'] ); % training save
-save_file_1 = fullfile(savedir, [ 'part_' sprintf('%03d', participant_nr) '_sess_' num2str(1) '_ambiguity_' num2str(AMBIGUITY)  '.mat'] );
-save_file_2 = fullfile(savedir, [ 'part_' sprintf('%03d', participant_nr) '_sess_' num2str(2) '_ambiguity_' num2str(AMBIGUITY)  '.mat'] );
+save_file_0 = fullfile(savedir, [ 'part_' sprintf('%03d', PARTICIPANT_NR) '_sess_' num2str(0) '_ambiguity_' num2str(AMBIGUITY)  '.mat'] ); % training save
+save_file_1 = fullfile(savedir, [ 'part_' sprintf('%03d', PARTICIPANT_NR) '_sess_' num2str(1) '_ambiguity_' num2str(AMBIGUITY)  '.mat'] );
+save_file_2 = fullfile(savedir, [ 'part_' sprintf('%03d', PARTICIPANT_NR) '_sess_' num2str(2) '_ambiguity_' num2str(AMBIGUITY)  '.mat'] );
 if exist(save_file_1, 'file')==2 || exist(save_file_2, 'file')==2; % check if savefiles exist
     display(' '); display('a logfile for this subjects already exists! do you want to overwrite?');
     overwrite = input('enter = no / ''yes'' = yes : ');
@@ -38,7 +39,7 @@ clear overwrite;
 
 % security check for settings
 disp(' '); disp('SETTINGS ARE:');
-disp(['participant number: ' num2str(participant_nr)]);
+disp(['participant number: ' num2str(PARTICIPANT_NR)]);
 if AMBIGUITY == 1;
     disp('ambiguity resolved for this subject: YES');
 else
@@ -47,7 +48,7 @@ end
 disp(' '); disp('PRESS ENTER TO CONTINUE...'); pause;
 
 % create replicable randomization
-randomisation = RandStream('mt19937ar', 'Seed', participant_nr + 10000*AMBIGUITY);
+randomisation = RandStream('mt19937ar', 'Seed', PARTICIPANT_NR + 10000*AMBIGUITY);
 RandStream.setGlobalStream(randomisation);
 clear randomisation;
 
@@ -77,8 +78,8 @@ clear continue_key kb_keycode;
 %% FINISH AND COPY LOGFILES
 
 fprintf('\nthe experiment is finished, please wait for files to by copied...');
-mkdir(target_path);
-copyfile(fullfile(savedir, '*'), fullfile(target_path));
+mkdir(TARGET_PATH);
+copyfile(fullfile(savedir, '*'), fullfile(TARGET_PATH));
 disp('done.');
 fprintf('\nselecting random trial for reward...');
 
