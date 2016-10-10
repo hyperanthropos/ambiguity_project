35a424df728ca9b078f4b32a1400c04246f52221
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2016-10-10 18:24:12 +0200

    "created clearer output for participants"

--- behavioral_pilot/create_reward_file.m
+++ behavioral_pilot/create_reward_file.m
@@ -19,21 +19,23 @@ diary(diary_file);
 diary on;
 
 % print content
+disp([ 'this is participant number: ' num2str(PARTICIPANT_NR) ]);
+disp(' ');
 disp('the following trial has been randomly selected:');
 disp([ 'session: ' num2str(reward_session) ' | trial: ' num2str(reward_trial) ]);
 disp(' ');
 disp('you had to decide between:');
+disp(' ');
 disp([ num2str(logrec(16,reward_trial)) 'CHF for sure.' ]);
 disp('OR');
 if logrec(7,reward_trial) == 1; % risky trial
     disp([ num2str( logrec(10,reward_trial)*100 ) '% chance to get ' num2str( logrec(12,reward_trial) ) 'CHF and ' num2str( logrec(11,reward_trial)*100 ) '% chance to get ' num2str( logrec(13,reward_trial) ) 'CHF' ]);
 elseif logrec(7,reward_trial) == 2; % ambiguous trial
-    disp([ 'an ambiguous amount of either ' num2str( logrec(14,reward_trial) ) 'CHF or ' num2str(logrec(14,reward_trial)) 'CHF' ]);
+    disp([ 'an ambiguous amount of either ' num2str( logrec(14,reward_trial) ) 'CHF or ' num2str(logrec(15,reward_trial)) 'CHF' ]);
 end
 disp(' ');
 if logrec(5,reward_trial) == 1 || 3; % fixed amount
     disp([ 'you decided for the ' num2str(logrec(16,reward_trial)) 'CHF for sure.']);
-    disp('so this is what you will get');
 elseif logrec(5,reward_trial) == 2; % risky amount
     disp('you decided for:');
     disp([ num2str( logrec(10,reward_trial)*100 ) '% chance to get ' num2str( logrec(12,reward_trial) ) 'CHF and ' num2str( logrec(11,reward_trial)*100 ) '% chance to get ' num2str( logrec(13,reward_trial) ) 'CHF' ]);
@@ -41,7 +43,7 @@ elseif logrec(5,reward_trial) == 2; % risky amount
     disp('please roll a dice to determine what you will get');
 elseif logrec(5,reward_trial) == 4; % ambiguous amount
     disp('you decided for:');
-    disp([ 'an ambiguous amount of either ' num2str( logrec(14,reward_trial)*100 ) 'CHF or ' num2str() 'CHF' ]);
+    disp([ 'an ambiguous amount of either ' num2str( logrec(14,reward_trial) ) 'CHF or ' num2str(logrec(15,reward_trial)) 'CHF' ]);
     disp('...which turns out to be:');
     disp([ num2str( logrec(10,reward_trial)*100 ) '% chance to get ' num2str( logrec(14,reward_trial) ) 'CHF and ' num2str( logrec(11,reward_trial)*100 ) '% chance to get ' num2str( logrec(15,reward_trial) ) 'CHF' ]);
     disp('please roll a dice to determine what you will get');
@@ -52,6 +54,7 @@ diary off;
 copyfile(diary_file, TARGET_PATH);
 % create an additional copy to be deleted after printed out in order to
 % stay in control when results come in quickly in a row
+if exist(fullfile(TARGET_PATH, 'delete'), 'dir') ~= 7; mkdir(fullfile(TARGET_PATH, 'delete')); end % create directory if it doesn't exist
 copyfile(diary_file, fullfile(TARGET_PATH, 'delete'));
 
 %% end function
