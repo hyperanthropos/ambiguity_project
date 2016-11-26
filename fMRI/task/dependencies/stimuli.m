f0bb2a9609cff77d2e10947e1d9657a4f751a75a
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2016-11-26 19:00:45 +0100

    "finished stimulus matrix except of timing"

--- fMRI/task/dependencies/stimuli.m
+++ fMRI/task/dependencies/stimuli.m
@@ -8,7 +8,6 @@ function [matrix, stim_nr] = stimuli( steps, diag, session, TIMING )
 % line 02 - session number
 % line 03 - stimulus number (randomized)
 % line 04 - trial type (1 = risky, 2 = ambigious)
-% line 05 - resolve ambiguity (1 = yes, 0 = no)
 %
 % line 06 - risk variance level (1-4; low to high variance)
 %               for risk: 25 at 80%, 33 at 60%, 50 at 40%, 100 at 20%
@@ -16,14 +15,14 @@ function [matrix, stim_nr] = stimuli( steps, diag, session, TIMING )
 %               for ambiguity: 15 vs 25, 10 vs 30, 5 vs 35, 0 vs 40
 % line 08 - counteroffer level (1-number of levels; low to high counteroffer)
 %
-% line 10 - option 1 - probability of offer [ line 6 ] (80%, 60%, 40%, 20%)
-% line 11 - option 1 - lower value risk [ line 6 ] (10.00 07.75 07.75 10.00 for risk)
-% line 12 - option 1 - upper value risk [ line 6 ] (22.50 28.17 38.37 60.00 for risk)
-% line 13 - option 1 - lower value ambiguity [ line 7 ] (15, 10, 5, 0 for ambigutiy)
-% line 14 - option 1 - upper value ambiguity [ line 7 ] (25, 30, 35, 40 for ambiguity)
-% line 15 - option 2 - counteroffer value [ line 8 ] (variable, matched to 20 expected value (EV)
+% line 10 - option 1 - probability of offer [ line 6 ] (80%, 65%, 50%, 35%, 20%)
+% line 11 - option 1 - lower value risk [ line 6 ] (17.5 12.3 10.0 9.7 11.3 for risk)
+% line 12 - option 1 - upper value risk [ line 6 ] (23.8 28.0 35.0 46.3 67.5 for risk)
+% line 13 - option 1 - lower value ambiguity [ line 7 ] (20 15 10 5 0 for ambigutiy)
+% line 14 - option 1 - upper value ambiguity [ line 7 ] (25 30 35 40 45 for ambiguity)
+% line 15 - option 2 - counteroffer value [ line 8 ] (variable, matched to 22.5 expected value (EV)
 %
-% line 20 - trial start time
+% line 20 - trial start time (actual decision is presented later after pre_time)
 % line 21 - position of counteroffer (left, right)
 % line 22 - position of higher offer (up or down) (higher offer = probabilistic in risky trials)
 %
@@ -37,50 +36,41 @@ function [matrix, stim_nr] = stimuli( steps, diag, session, TIMING )
 DIAG = diag;                % run diagnostics of stimuli range
 
 ITI = TIMING.iti;           % mean ITI between trials
-STIMULUS_TIME = TIMING.pre_time + TIMING.duration + TIMING.indication;
 
 X.steps = steps;                % steps of counteroffer value (this must be matched to levels of risk and ambiguity)
-                                    % maximum counteroffer (risk vs. ambiguity): 22.5 vs 25, 28.2 vs 30, 38.4 vs 35, 60 vs 40
-                                    % minimum counteroffer (risk vs. ambiguity): 10 vs 15, 7.8 vs 10, 7.8 vs 5, 10 vs 0
-                                    % combined: 15 - 22.5; 10 - 28.2; 7.8 - 35; 10 - 40; 
-                                    % in % [0.750 1.125]; [0.500 1.410]; [0.390 1.750]; [0.500 2.000];
-X.var{1} = [0.750 1.125];                                         % for variance level 1: 75% - 125% (15 - 25)
-X.var{2} = [0.500 1.330];                                         % for variance level 2: 50% - 150% (10 - 30)
-X.var{3} = [0.390 1.540];                                         % for variance level 3: 25% - 175% (5 - 35)
-X.var{4} = [0.500 1.750];                                         % for variance level 4: 0% - 200% (0 - 40)
-X.var{5} = [0.500 1.750];
-
-% ALL GOOD BUT ABOVE HERE (fix ranges, delete old data)
-
-X.RVH = [95/4, (15*91^(1/2))/26+45/2, 35, (5*91^(1/2))/2+45/2, 135/2];                 % probability values high
-% 23.8 28.0 35.0 46.3 67.5
-X.RVL = [35/2, 45/2-(15*91^(1/2))/14, 10, 45/2-(35*91^(1/2))/26, 45/4];                % probability values low
-% 17.5 12.3 10.0 9.7 11.3
-
+                                    % maximum counteroffer (risk vs. ambiguity): 23.8 vs 25, 28.0 vs 30, 35.0 vs 35, 46.3 vs 40, 67.5 vs 45
+                                    % minimum counteroffer (risk vs. ambiguity): 17.5 vs 20, 12.3 vs 15, 10.0 vs 10, 9.7 vs 5, 11.3 vs 0  
+                                    % combined: 20 - 23.8; 15 - 28.0; 10.1 - 35.0; 9.7 - 40; 11.3 - 45;
 
 X.RPH = [.8 .65 .5 .35 .2];                                                 % risky probabilities for high offer
 X.RVH = [95/4, (15*91^(1/2))/26+45/2, 35, (5*91^(1/2))/2+45/2, 135/2];      % risky value levels high
-% 23.8 28.0 35.0 46.3 67.5
+                                                                                    % 23.8 28.0 35.0 46.3 67.5
 X.RPL = [.2 .35 .5 .65 .8];                                                 % risky probabilities for low offers
 X.RVL = [35/2, 45/2-(15*91^(1/2))/14, 10, 45/2-(35*91^(1/2))/26, 45/4];     % risky value levels low
-% 17.5 12.3 10.0 9.7 11.3
+                                                                                    % 17.5 12.3 10.0 9.7 11.3
 X.RN = 5;                                                                   % number of risky levels
 X.AVL = [20 15 10 5 0];                                                     % ambiguitly levels low
 X.AVH = [25 30 35 40 45];                                                   % ambiguitly levels high
 X.AN = 5;                                                                   % number of ambiguous levels
 
-% X.RPH = [.8 .6 .4 .2];                                          % risky probabilities for high offer
-% X.RVH = [22.5 , (10*6^(1/2))/3+20 , (15*6^(1/2))/2+20 , 60];    % risky value levels high
-% X.RPL = 1-X.RPH;                                                % risky probabilities for low offers
-% X.RVL = [10 , 20-5*6^(1/2) , 20-5*6^(1/2) , 10];                % risky value levels low
-% X.RN = 4;                                                       % number of risky levels
-% X.AVL = [15 10 5 0];                                            % ambiguitly levels low
-% X.AVH = [25 30 35 40];                                          % ambiguitly levels high
-% X.AN = 4;                                                       % number of ambiguous levels
-
 X.EV = 22.5;                                                      % expected value (has to matched to X.RPH, X.RVH, X.AVL, X.AVH)
 
 stim_nr = (X.RN+X.AN)*X.steps;
+
+% create percentage value of highest / lowest possible counteroffer
+% in realtion to expected value, so that the counteroffer is never lower
+% than the low uncertain or higher than the high uncertain amount
+% (assuming risk and ambiguity have the same number of variance levels)
+perc_bound(1,:) = max([X.RVL;X.AVL])/X.EV; % maximum of lowest offer divided by EV
+perc_bound(2,:) = min([X.RVH;X.AVH])/X.EV; % minimum of highest offer divided by EV
+
+% result: min/max percent for variance levels 1 to 5
+% [0.889 1.055] [0.667 1.244] [0.445 1.555] [0.430 1.778] [0.501 1.999]
+
+% set variance later used to scale counteroffers
+for i = 1:X.RN;
+    X.var{i} = perc_bound(:,i);
+end
 
 %% DIAGNOSTIC: COMPARE MEAN VARIANCE APPROACH TO UTILITY FUNCTIONS
 
@@ -187,7 +177,7 @@ if DIAG == 1;
     % --- --- --- END SKIP THIS SECTION --- --- --- %
 end
 
-%% CREATE MATRIX TO REPEAT
+%% CREATE MATRIX
 
 trials_risky = 1:X.RN*X.steps;
 trials_ambiguous = X.RN*X.steps+1:X.RN*X.steps+X.AN*X.steps;
@@ -202,16 +192,16 @@ r_matrix(7,trials_risky) = repmat(1:X.AN, 1, X.steps);                    % line
 r_matrix(7,trials_ambiguous) = kron(1:X.AN, ones(1,X.steps));             % line 07 - ambiguity variance level (1-4; low to high variance)
 r_matrix(8,:) = repmat(1:X.steps, 1, X.AN+X.RN);                          % line 08 - counteroffer level (1-number of levels; low to high counteroffer)
 
-r_matrix(10,trials_risky) = kron(X.RPH, ones(1,X.steps));                 % line 10 - option 1 - probability of offer [ line 6 ] (80%, 60%, 40%, 20%)
-r_matrix(10,trials_ambiguous) = repmat(X.RPH, 1, X.steps);                % line 10 - option 1 - probability of offer [ line 6 ] (80%, 60%, 40%, 20%)
-r_matrix(11,trials_risky) = kron(X.RVL, ones(1,X.steps));                 % line 11 - option 1 - lower value risk [ line 6 ] (always 0 for risk)
-r_matrix(11,trials_ambiguous) = repmat(X.RVL, 1, X.steps);                % line 11 - option 1 - lower value risk [ line 6 ] (always 0 for risk)
-r_matrix(12,trials_risky) = kron(X.RVH, ones(1,X.steps));                 % line 12 - option 1 - upper value risk [ line 6 ] (25, 33, 50, 100 for risk)
-r_matrix(12,trials_ambiguous) = repmat(X.RVH, 1, X.steps);                % line 12 - option 1 - upper value risk [ line 6 ] (25, 33, 50, 100 for risk)
-r_matrix(13,trials_risky) = repmat(X.AVL, 1, X.steps);                    % line 13 - option 1 - lower value ambiguity [ line 7 ] (15, 10, 5, 0 for ambigutiy)
-r_matrix(13,trials_ambiguous) =  kron(X.AVL, ones(1,X.steps));            % line 13 - option 1 - lower value ambiguity [ line 7 ] (15, 10, 5, 0 for ambigutiy)
-r_matrix(14,trials_risky) = repmat(X.AVH, 1, X.steps);                    % line 14 - option 1 - upper value ambiguity [ line 7 ] (25, 30, 35, 40 for ambiguity)
-r_matrix(14,trials_ambiguous) =  kron(X.AVH, ones(1,X.steps));            % line 14 - option 1 - upper value ambiguity [ line 7 ] (25, 30, 35, 40 for ambiguity)
+r_matrix(10,trials_risky) = kron(X.RPH, ones(1,X.steps));                 % line 10 - option 1 - probability of offer [ line 6 ]
+r_matrix(10,trials_ambiguous) = repmat(X.RPH, 1, X.steps);                % line 10 - option 1 - probability of offer [ line 6 ]
+r_matrix(11,trials_risky) = kron(X.RVL, ones(1,X.steps));                 % line 11 - option 1 - lower value risk [ line 6 ]
+r_matrix(11,trials_ambiguous) = repmat(X.RVL, 1, X.steps);                % line 11 - option 1 - lower value risk [ line 6 ]
+r_matrix(12,trials_risky) = kron(X.RVH, ones(1,X.steps));                 % line 12 - option 1 - upper value risk [ line 6 ]
+r_matrix(12,trials_ambiguous) = repmat(X.RVH, 1, X.steps);                % line 12 - option 1 - upper value risk [ line 6 ]
+r_matrix(13,trials_risky) = repmat(X.AVL, 1, X.steps);                    % line 13 - option 1 - lower value ambiguity [ line 7 ]
+r_matrix(13,trials_ambiguous) =  kron(X.AVL, ones(1,X.steps));            % line 13 - option 1 - lower value ambiguity [ line 7 ]
+r_matrix(14,trials_risky) = repmat(X.AVH, 1, X.steps);                    % line 14 - option 1 - upper value ambiguity [ line 7 ]
+r_matrix(14,trials_ambiguous) =  kron(X.AVH, ones(1,X.steps));            % line 14 - option 1 - upper value ambiguity [ line 7 ]
 
 % create counteroffers
 counteroffers = [];
@@ -247,9 +237,9 @@ if DIAG == 1;
     
     subplot(1,2,2);
     scatter(r_matrix(7,trials_ambiguous), r_matrix(15,trials_ambiguous)/X.EV); box off; hold on;
-    plot(SV.mvar(2,:)./20, 'k--', 'linewidth', 2);
-    plot(SV.hyp(2,:)./20, 'r--', 'linewidth', 2);
-    plot(SV.pros(2,:)./20, 'b--', 'linewidth', 2);
+    plot(SV.mvar(2,:)./X.EV, 'k--', 'linewidth', 2);
+    plot(SV.hyp(2,:)./X.EV, 'r--', 'linewidth', 2);
+    plot(SV.pros(2,:)./X.EV, 'b--', 'linewidth', 2);
     
     legend('single trial', 'mvar', 'hyp', 'pros', 'location', 'northwest');
     axis([.5 5.5 -.1 2.1]); xlabel('variance'); ylabel('expected value ratio');
@@ -260,8 +250,6 @@ end
 
 %% RANDOMIZE AND FINALIZE MATRIX 
 
-% GOOD TILL HERE (CREATE TIMING)
-
 % randomize r_matrix
 matrix = r_matrix(:,randperm(stim_nr));    
 
@@ -269,17 +257,46 @@ matrix = r_matrix(:,randperm(stim_nr));
 matrix(1,:) = 1:stim_nr;                                        % line 01 - presentation number (sequential)
 matrix(2,:) = ones(1,stim_nr)*session;                          % line 02 - session number
 
-% create trial start time
-matrix(20,:) = ones(1,stim_nr)*ITI;                             % line 20 - ITI (time until next decision)
-
 % fill unused lines with NaN for security
 matrix(5,:) = NaN(1,stim_nr);
 matrix(9,:) = NaN(1,stim_nr);
 matrix(16:19,:) = NaN(4,stim_nr);
 
-% derandomize
-% sorted_matrix = sortrows(matrix', [2 3])';
+%% CREATE TRIAL ONSET TIMING
 
+% stimdur = TIMING.pre_time + TIMING.duration + TIMING.indication;
+% 
+% gammafactor = 1;
+% gammavec = gamrnd(gammafactor, var_iti/gammafactor, 1, 99);
+% offset = var_iti/mean(gammavec);
+% gammavec = gammavec*offset;
+% 
+% trialstart(1,:) = 0;
+% 
+% % add 10 3s null events
+% x = randperm(99);
+% null_location = x(1:10);
+% for i = 1:99
+%     if find(null_location==i)
+%         trialstart(i+1) = trialstart(i)+prepcross+stimdur+indication+gammavec(i)+3;
+%     else
+%         trialstart(i+1) = trialstart(i)+prepcross+stimdur+indication+gammavec(i);
+%     end
+% end
+% 
+% % % % without null events
+% % % for i = 1:99
+% % % trialstart(i+1) = trialstart(i)+prepcross+stimdur+indication+gammavec(i);
+% % % end
+% 
+% duration = trialstart(end)+prepcross+stimdur+indication;
+% 
+% disp(duration)
+
+matrix(20,:) = ones(1,stim_nr)*ITI;                             % line 20 - ITI (time until next decision)
+
+%% DERANDOMIZE MATRIX
+% sorted_matrix = sortrows(matrix', [2 3])';
 
 %% end function code
 end
