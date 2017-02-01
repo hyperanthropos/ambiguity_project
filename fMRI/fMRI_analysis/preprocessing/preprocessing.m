9984e7fb7b565410bf019f4d20a1242ce4b8e75b
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2017-02-01 20:27:24 +0100

    "added more parameter options + parallel computing capability"

--- fMRI/fMRI_analysis/preprocessing/preprocessing.m
+++ fMRI/fMRI_analysis/preprocessing/preprocessing.m
@@ -1,12 +1,13 @@
 %% code to preprocess fMRI data
 % uses spm12 toolbox
 % needs a SPM preprocessing batch to work with
+% must have fMRI data sorted into subject folders (see file_sorting.sh)
 
 %% SETUP
 clear; close all;
 
 % where is the data to be processed stored:
-DATA_FOLDER = '/home/fridolin/DATA/EXPERIMENTS/04_Madeleine/DATA/fMRI/fMRI_images/prepro_test';
+DATA_FOLDER = '/home/fridolin/DATA/EXPERIMENTS/04_Madeleine/DATA/fMRI/fMRI_images/preprocessed';
 % which batch should be used for processing (must be in a "batch" subfolder):
 BATCH = 'prepro_batch.mat';
 % which subjects should be processed:
@@ -18,11 +19,20 @@ DIR.SPM.spmpath = '/home/fridolin/DATA/MATLAB/SPM/spm12b/'; % path of SPM 12
 DIR.SPM.tissuemaps = '/home/fridolin/DATA/MATLAB/SPM/spm12b/tpm/';
 
 % PARAMETERS
-P.TR = 2.372; % time to repeat
+% timing (in seconds)
+P.TR = 2.370; % time to repeat
 P.SLICES = 40; % number of slices
+% spatial (in mm: x,y,z)
+P.SMOOTHING = [5 5 5]; % FWHM gaussian kernel in mm (x,y,z)
+P.VOX_FUNCT = [3 3 3]; % set voxel size for functional images
+P.VOX_STRUCT = [1 1 1]; % set voxel size for structural image
+
+% PROCESSING PREFERENCES
+JUST_SAVE = 0; % just create and save the batches for all subjects - do not compute yet
+WORKERS = 6; % how many parallel instances to compute
 
 % access current matlabbatch via SPM:
-% spm_jobman('interactive',matlabbatch);
+%   spm_jobman('interactive',matlabbatch);
 
 %% PREPARE PREPROCESSING
 
@@ -32,6 +42,7 @@ DIR.data = DATA_FOLDER; clear DATA_FOLDER;
 DIR.batch = fullfile(DIR.home, 'batch');
 DIR.savedir = fullfile(DIR.batch, 'used_batches');
 if exist(DIR.savedir, 'dir') ~= 7; mkdir(DIR.savedir); end
+delete(fullfile(DIR.savedir, '*'));
 
 % START SPM AND PREPARE BATCH
 addpath(DIR.SPM.spmpath);
@@ -54,6 +65,10 @@ matlabbatch{1, 2}.spm.temporal.st.tr = P.TR;
 matlabbatch{1, 2}.spm.temporal.st.ta = P.TR-(P.TR/P.SLICES);
 matlabbatch{1, 2}.spm.temporal.st.so = 1:P.SLICES; % assuming ascending aquisition
 matlabbatch{1, 2}.spm.temporal.st.refslice = round(P.SLICES/2);
+% set spatial parameters
+matlabbatch{1, 8}.spm.spatial.smooth.fwhm = P.SMOOTHING;
+matlabbatch{1, 7}.spm.spatial.normalise.write.woptions.vox = P.VOX_FUNCT;
+matlabbatch{1, 9}.spm.spatial.normalise.write.woptions.vox = P.VOX_STRUCT;
 
 % save batch for subject loop
 save(fullfile(DIR.savedir, 'base_batch.mat'), 'matlabbatch');
@@ -90,10 +105,16 @@ for sub = SUBJECTS
    
 end
 
-% run batches
-parfor sub = SUBJECTS
-    spm_jobman('initcfg');
-    spm_jobman('run',parallel_store{sub});
+% start preprocessing
+if JUST_SAVE ~= 1;
+    % start parallel processing
+    parpool(WORKERS);
+    
+    % run batches
+    parfor sub = SUBJECTS
+        spm_jobman('initcfg');
+        spm_jobman('run',parallel_store{sub});
+    end
 end
 
 clear subcode filekeeper matlabbatch sub;
