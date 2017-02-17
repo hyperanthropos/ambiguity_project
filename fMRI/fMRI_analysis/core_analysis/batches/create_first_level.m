86894e1e79be964b7c42cce8d287af0f42929ebc
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2017-02-17 23:50:22 +0100

    "base first level batch"

---
+++ fMRI/fMRI_analysis/core_analysis/batches/create_first_level.m
@@ -0,0 +1,41 @@
+function [ matlabbatch ] = create_first_level( )
+% function to create first level batch
+%   creates basic batch to be modified for each subject
+
+matlabbatch{1}.spm.stats.fmri_spec.dir = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
+matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 2.37;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 40;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 20;
+matlabbatch{1}.spm.stats.fmri_spec.sess.scans = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).name = 'risk';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).onset = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).duration = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).tmod = 0;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).orth = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).name = 'ambiguity';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).onset = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).duration = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).tmod = 0;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).orth = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi = {''};
+matlabbatch{1}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi_reg = {''};
+matlabbatch{1}.spm.stats.fmri_spec.sess.hpf = 128;
+matlabbatch{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
+matlabbatch{1}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
+matlabbatch{1}.spm.stats.fmri_spec.volt = 1;
+matlabbatch{1}.spm.stats.fmri_spec.global = 'None';
+matlabbatch{1}.spm.stats.fmri_spec.mthresh = 0.8;
+matlabbatch{1}.spm.stats.fmri_spec.mask = {''};
+matlabbatch{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('fMRI model specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess = {};
+matlabbatch{3}.spm.stats.con.delete = 1;
+
+end
