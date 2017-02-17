e5e1d6c3056a190006489a7be8cad1dc5bff640c
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2017-02-17 23:50:07 +0100

    "base second level batch"

---
+++ fMRI/fMRI_analysis/core_analysis/batches/create_second_level.m
@@ -0,0 +1,44 @@
+function [ matlabbatch ] = create_second_level( )
+% function to create second level batch
+%   creates basic batch to be modified for different onsets/pmods
+
+matlabbatch{1}.spm.stats.factorial_design.dir = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.des.pt.pair.scans = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.des.pt.gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.pt.ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'risk pos';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [1 0];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'risk neg';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = [-1 0];
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.name = 'ambi pos';
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.weights = [0 1];
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.name = 'ambi neg';
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.weights = [0 -1];
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{5}.tcon.name = 'risk > ambi';
+matlabbatch{3}.spm.stats.con.consess{5}.tcon.weights = [1 -1];
+matlabbatch{3}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{6}.tcon.name = 'ambi > risk';
+matlabbatch{3}.spm.stats.con.consess{6}.tcon.weights = [-1 1];
+matlabbatch{3}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{7}.fcon.name = 'effects of interest';
+matlabbatch{3}.spm.stats.con.consess{7}.fcon.weights = [1 0; 0 1];
+matlabbatch{3}.spm.stats.con.consess{7}.fcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 1;
+
+end
