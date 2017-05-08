6c6edce488effd418386d401c669f14bdadaf22c
Sebastian Weissengruber <sebastian.weissengruber@gmail.com>
2017-05-08 19:01:55 +0200

    "added support for SV calculation - now need proper trials specification"

--- behavioral/behavioral_modeling/model_predictions.m
+++ behavioral/behavioral_modeling/model_predictions.m
@@ -22,7 +22,9 @@ EV_LEVELS = 2; % how many steps of expected value variation
 EV = [8.5 34]; % what were the expected values of all gambles
 
 % specific trials
-TRIALS.type = [1];
+nTrials = VAR_NR*EV_LEVELS;
+
+TRIALS.type = [ones(1,VAR_NR),ones(1,VAR_NR)*2];
 TRIALS.prob_h = [1];
 TRIALS.val_h = [1];
 TRIALS.prob_l = [1];
@@ -34,20 +36,33 @@ TRIALS.mat(3,:) = TRIALS.val_h; % high amount
 TRIALS.mat(4,:) = TRIALS.prob_l; % prob. of low amount
 TRIALS.mat(5,:) = TRIALS.val_l; % low amount
 
-
 %% DEFINE UTILITY FUNCTIONS AND PARAMETERS
 
-% which decision process can be explained with which theory
-% --> needs proper model comparison
+% set utility function and calculate expected value
+utility_function_risk = 'hyperbolic';
+utility_function_ambi = 'hyperbolic';
 
-sv = [];
+% set paramters for functions
+funparam.hyperbolic.param1.risk = [];
+funparam.hyperbolic.param1.ambi = [];
 
-% + softmax
-% --> use fmincon
-
-
-
-
+% calculate subjective value for all trials
+sv = NaN(1,nTrials);
+for iTrial = 1:nTrials
+    if TRIALS.type(iTrial) == 1;
+        utility_function = utility_function_risk;
+    elseif TRIALS.type(iTrial) == 2;
+        utility_function = utility_function_ambi;
+    end
+    switch utility_function
+        case 'hyperbolic'
+            sv(iTrial) = 1;
+        case 'prospect'
+            sv(iTrial) = 2;
+        otherwise
+            error('utitity function not found - check spelling');
+    end
+end
 
 %% PLOT ACTUAL RESULTS FROM EXPERIMENT 2 AS REFERENCE
 
@@ -57,6 +72,8 @@ sv = [];
 PART = 1:52;
 EXCLUDE = false;
 EXCLUDE_SUBS = [];
+
+%%% --- PLOT RESULTS
 
 % exclude subjects from subject vector
 exclude_vec = EXCLUDE_SUBS;
@@ -68,58 +85,36 @@ clear i exclude_vec;
 % load data
 load(fullfile(DIR.input, 'parameters.mat'), 'PARAM');
 
-%%% --- PLOT RESULTS
-
-% uses 5D matrix of premium paramters   (var_level,ev_level,type,sub,[repeat])
-
 % set axes (multiples of ev for y axis)
-axis_scale = [.5 VAR_NR+.5 0.5 1.1 ];
+axis_scale = [.5 VAR_NR+.5 0 1 ];
 
 % draw figure
-FIGS.fig2_1 = figure('Name', 'F3: group summary', 'Color', 'w', 'units', 'normalized', 'outerposition', [0 .5 .6 .5]);
-FIGS.fig2_2 = figure('Name', 'F3.1: correlation for different variance levels', 'Color', 'w', 'units', 'normalized', 'outerposition', [0 0 .6 .4]);
-
+FIGS.fig1 = figure('Name', 'model predictions', 'Color', 'w', 'units', 'normalized', 'outerposition', [.0 .5 .6 .5]);
 for ev_level = 1:EV_LEVELS;
     
     new_axis = axis_scale.*[ 1 1 EV(ev_level) EV(ev_level) ];
     
     % prepare data to plot
+    % uses 5D matrix of premium paramters   (var_level,ev_level,type,sub,[repeat])
     data = PARAM.premiums.ce(:,ev_level,:,PART);
-    
-    % mean preference over all variance levels
-    data_persub = mean(mean(data, 1),2);
-    x = squeeze(data_persub); % x(1,:) = risk; x(2,:) = ambiguity
     
     % mean data of all repeats (var, risk/ambi, sub)
     data_allrep = mean(data, 2);
     y = squeeze(data_allrep);
     
-    % --- FIGURE 2.1
-    figure(FIGS.fig2_1);
-    
-    % --- PANEL 3: preference for different variance levels
-    subplot(2,2,2+2*(ev_level-1));
-    bar_or_line = 'line';
-    switch bar_or_line
-        case 'line';
-            h = errorbar(mean(y, 3), std(y, 1, 3)./(size(PART,2))^.5, 'LineWidth', 2); hold on; box off;
-            set(h(1), 'Color', [.0 .0 .8]); set(h(2), 'Color', [.8 .0 .0]);
-        case 'bar';
-            h = barwitherr(std(y, 1, 3)./(size(PART,2))^.5, mean(y, 3)); hold on; box off;
-            set(h(1), 'FaceColor', [.0 .0 .8]); set(h(2), 'FaceColor', [.8 .0 .0]);
-    end
+    % plot the data
+    subplot(1,2,ev_level);
+    h = errorbar(mean(y, 3), std(y, 1, 3)./(size(PART,2))^.5, 'LineWidth', 2); hold on; box off;
+    set(h(1), 'Color', [.0 .0 .8]); set(h(2), 'Color', [.8 .0 .0]);
     clear bar_or_line;
     plot( ones(1,VAR_NR)*EV(ev_level), '--k' , 'LineWidth', 2 );
     axis(new_axis); axis('auto x'); ylabel('subjective value');
-    legend('risk', 'ambiguity');
-    set(gca, 'xtick', 1:VAR_NR );
+    legend('risk', 'ambiguity', 'neutrality', 'Location', 'southwest');
+    legend('boxoff'); set(gca, 'xtick', 1:VAR_NR );
     xlabel('variance');
     
 end
-
-clear PARAM;
-
-% clear data data_persub data_persub_var data_allrep x y varlevel axis_scale new_axis h;
+clear PARAM axis_scale new_axis y h data data_allrep ev_level;
 
 %% PLOT UTILTY FUNCTION OVER STIMULI SPACE
 
